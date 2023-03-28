0de5d3883a97058788cab47901c7d1c742597cca
judah <landzbej@gmail.com>
2023-03-28 19:30:34 +0800

    second commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// const express = require("express");
+// const routes = require('./routes/index');
+
+// const app = express();
+// const port = 3000;
+
+// app.use('',routes);
+// app.listen(port, () => console.log(`app is running on ${port}!`));
+
+const express = require("express");
+
+const app = express();
+const host = 'localhost';
+const port = 3000;
+
+// app.set("views", "./views");
+// app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// Listener
+app.listen(port, host, () => {
+  console.log(`Budget is listening on port ${port} of ${host}!`);
+});
